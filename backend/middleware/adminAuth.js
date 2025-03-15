20780890e97978a7b3db22eb18da2b5c0b57fdbf
shiva <shivaprasadpaka5@gmail.com>
2025-03-15 22:52:03 +0530

    Fix Admin JWT  verify

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -4,12 +4,12 @@ const adminAuth = (req, res, next) => {
         const { token } = req.headers;
 
         if (!token) {
-            return res.json({ success: false, message: "Not Authorized. Login Again" });
+            return res.json({ success: false, message: "Not Authorized " });
         }
 
         const token_decode = jwt.verify(token, process.env.JWT_SECRET);
 
-        if (token_decode !== process.env.ADMIN_EMAIL) {
+        if (token_decode.email !== process.env.ADMIN_EMAIL) {
             return res.json({ success: false, message: "Not Authorized. Login Again" });
         }
 
